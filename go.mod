8156c750ba3cd26b9e12f4cbb9e64a6da63f627b
Kevin Mulvey <kmulvey@linux.com>
2022-06-22 13:10:17 -0600

    simple integration test

--- go.mod
+++ go.mod
@@ -5,9 +5,13 @@ go 1.18
 require (
 	github.com/kmulvey/imageconvert v1.0.2
 	github.com/sirupsen/logrus v1.8.1
+	github.com/stretchr/testify v1.6.1
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/image v0.0.0-20220601225756-64ec528b34cd // indirect
 	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200605160147-a5ece683394c // indirect
 )
---
+++ integration_test.go
@@ -0,0 +1,22 @@
+package imageupsizer
+
+import (
+	_ "image/jpeg"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEverything(t *testing.T) {
+	var originalImage, err = GetImageConfigFromFile("./test.jpg")
+	assert.NoError(t, err)
+
+	largerImage, err := GetLargerImageFromFile("./test.jpg", "./")
+	assert.NoError(t, err)
+
+	assert.True(t, largerImage.Area > originalImage.Area)
+	assert.True(t, largerImage.FileSize > originalImage.FileSize)
+
+	assert.NoError(t, os.Remove(largerImage.LocalPath))
+}
